c400c2bbac973f3aed3c6d4a1a677ed71e214c32
Adam-Smart <adams8632@hotmail.co.uk>
2023-08-04 11:40:51 +0100

    CRUD for Products

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -46,15 +46,15 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-  try { 
-    const categoryData = await Category.update
-    ({ category_name: req.params.genre},req.body);
-    res.status(200).json(categoryData);
-    console.log(`Updated: ${categoryData}`);
-  } catch (err) {
-    console.log("something went wrong!")
-    res.status(500).json({message: "Something went wrong"})
-  }
+  // try { 
+  //   const categoryData = await Category.update
+  //   ({ category_name: req.params.category_name},req.body);
+  //   res.status(200).json(categoryData);
+  //   console.log(`Updated: ${categoryData}`);
+  // } catch (err) {
+  //   console.log("something went wrong!")
+  //   res.status(500).json({message: "Something went wrong"})
+  // }
 });
 
 router.delete('/:id', async (req, res) => {
