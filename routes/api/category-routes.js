88a45a1db19537af0a939fbc9cd98e15a78a0fd0
Adam-Smart <adams8632@hotmail.co.uk>
2023-08-04 13:45:43 +0100

    fixed update request for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -47,8 +47,22 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-
-})
+  try{
+    const categoryData = await Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {where: { id: req.params.id}
+    });
+    if(!categoryData){
+      res.status(404).json({message : "There is no category with this id"});
+      return
+    }
+    res.status(200).json(categoryData);
+  }catch (err) {
+    res.status(500).json(err);
+};
+});
   // update a category by its `id` value
 
 router.delete('/:id', async (req, res) => {
